f3df4975cdfe7fa0aa27cd65a67f77aed8f1c620
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-05-02 09:45:01 -0300

    Update Playlists.js

--- src/Components/Playlists.js
+++ src/Components/Playlists.js
@@ -1,11 +1,21 @@
 import React from 'react'
-import 'Playlist.css'
+import './Playlists.css'
+
+const Nome = [
+  {id: 1, autor: 'Mitti'}
+];
 
 const Playlists = () => {
   return (
     <div>
       <div>
-        
+        <div className='banner'>
+          <h1>Sou Triste e DECADENTE</h1>
+          {Nome.map((music) => (
+            <p key={music.id} className='autor'>Por: {music.autor}</p>
+          ))}
+          <p className='numeroMusicas'></p>
+        </div>
       </div>
     </div>
   )
