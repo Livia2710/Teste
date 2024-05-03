4fa7a8b56bdbaa043a5cc24a80bf94820c0c9a08
Mitsuaki Tanaka <130664643+mitsuTanak@users.noreply.github.com>
2024-05-03 14:22:05 -0300

    adx

--- src/Components/Player.js
+++ src/Components/Player.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Player = () => {
   return (
     <div>
-        <h1>Player</h1>
+
+
+
     </div>
   )
 }
--- src/Components/Playlists.js
+++ src/Components/Playlists.js
@@ -19,15 +19,6 @@ const PlaylisInfos = [
 ];
 
 
-  {id: 2, cover: Capa1, name: 'Only One Who Knows', author: 'Arctic Monkeys', durantion: '3:04', song: mp3_1},
-  {id: 3, cover: Capa2, name: 'Glimpse of Us', author: 'Joji', durantion: '3:53', song: mp3_2},
-  {id: 4, cover: Capa3, name: 'Sparks', author: 'Coldplay', durantion: '3:47', song: mp3_3},
-  {id: 5, cover: Capa4, name: 'Grenade', author: 'Bruno Mars', durantion: '3:42', song: mp3_4},
-  {id: 6, cover: Capa5, name: 'Rises the Moon', author: 'Liana Flores', durantion: '2:41', song: mp3_5},
-  {id: 7, cover: Capa6, name: 'De 10 Mulher 11 é Maluca', author: 'Mc Saci', durantion: '1:47', song: mp3_6},
-  {id: 8, cover: Capa7, name: 'Fogo Fátuo', author: 'Lupe De Lupe', durantion: '3:53', song: mp3_7},
-]
-
 const ImageSwap = () => {
   const [image, setImage] = useState(SaveBefor);
 
